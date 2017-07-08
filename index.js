9d3d8354c258e7f975a748b37a589b9ac52affec
Joseph Broderick <PenguinMan98@usa.net>
2017-07-08 07:27:35 -0600

    Working on the playerList system

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const { app, BrowserWindow, ipcMain } = electron;
 const gun = new Gun('https://gun-gfcvuhvicq.now.sh/gun');
 const fiveMinutes = 1000 * 60 * 5;
 let mainWindow, thisAction;
+let sessionID = parseInt(Math.random() * 1000000000 );
 
 /* ==== GUN Aliases ==== */
 let gMoleapp = gun.get('moleapp');
-let gMyself = gMoleapp.get('myself');
+//let gMyself = gMoleapp.get('myself');
 let gPlayerList = gMoleapp.get('player_list');
 let gGameData = gMoleapp.get('game_data');
 let gHoles = gGameData.get('holes');
 let gMoles = gGameData.get('moles');
+
+//gPlayerList.put({}); // debug clear player list
 
 /* ==== Main program entry point ==== */
 app.on('ready', function(){
@@ -20,15 +23,26 @@ app.on('ready', function(){
 
   /* ==== GUN Listeners ==== */
   // Listen for updates to the player_list
-  /*gPlayerList.on( (data, key) => {
-    console.log('player list updates received!', data, key);
-  });*/
+  gPlayerList.map().val((value, key)=>{
+    console.log('playerList Update', value, key);
+    mainWindow.webContents.send('player:update',{
+      name: value.name,
+      score: value.score,
+      playerId: key,
+      mySessionId: sessionID
+    });
+  });
   // Listen for my player
-  gMyself.on((data, key) => {
+  /*gMyself.on((data, key) => {
     console.log('I got me!', data, key);
+    // when I've loaded myself,
+    gPlayerList.get(sessionID).put({
+      'name': data.name,
+      'lastAction': Date.now()
+    });
 
     mainWindow.webContents.send('myself:update',data);
-  });
+  });*/
   // Listen for hole changes
   gHoles.map((value, key)=>{
     console.log('I got a hole update!',value,key);
@@ -37,11 +51,25 @@ app.on('ready', function(){
 
 });
 
-// Do something when a hole is clicked
+// Do something when my username is set
 ipcMain.on('myself:put',function(event,data){
   console.log('Setting Username', data);
-  gMyself.put({'name':data.username, 'thisAction':Date.now()});
+  //gMyself.put({'name':data.username});
+  gPlayerList.get(sessionID).put({
+    'name': data.username,
+    'lastAction': Date.now(),
+    'score': 0,
+    'sessionId': sessionID
+  });
 });
+// Do something when a playerlist is requested
+ipcMain.on('playerlist:get',function(event,data){
+  console.log('playerlist requested', data);
+  gPlayerList.val((data)=>{
+    console.log('gPlayerList should be here',data);
+  });
+});
+// Do something when a hole is clicked
 ipcMain.on('hole:click',function(event,data){
   console.log('hole clicked', data);
   //gHoles.get(data.id).put({'bgColor':data.bgColor});
